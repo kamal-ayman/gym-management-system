b111c631b793298efae3d84b73c6ef62c12c1107
kamal <kamal.ayman522@gmail.com>
2023-12-19 18:50:39 +0200

    update db file

--- db/main.sql
+++ db/main.sql
@@ -1,14 +1,40 @@
-create table member(
-	id int not null primary key unique,
-	first_name char,
-	mid_name char,
-	last_name char,
-	gender int,
-	age int,
-	phone char,
-	email char,
-	subscription_type int,
-	subscription_price double,
-    start_date date,
-    end_date date
+CREATE TABLE admin (
+    id BIGINT PRIMARY KEY,
+    name TEXT NULL,
+    phone TEXT NULL,
+    address TEXT NULL,
+    password TEXT NULL
+);
+  
+  CREATE TABLE trainers (
+    id BIGINT PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    gender INT NOT NULL,
+    experience INT NOT NULL,
+    age INT NOT NULL,
+    work_time INT NOT NULL,
+    email TEXT NOT NULL
+);
+  
+  CREATE TABLE member (
+    id BIGINT PRIMARY KEY,
+    name TEXT NULL,
+    gender INT NULL,
+    age INT NULL,
+    phone TEXT NULL,
+    email TEXT NULL,
+    sub_type INT NULL,
+    sub_price DOUBLE PRECISION NULL,
+    reg_st TEXT NULL,
+    reg_ed TEXT NULL
+);
+
+CREATE TABLE machines (
+    sn BIGINT PRIMARY KEY,
+    name INT NOT NULL,
+    training_musicale INT NOT NULL,
+    trade_mark TEXT NOT NULL,
+    power INT NOT NULL
 );
